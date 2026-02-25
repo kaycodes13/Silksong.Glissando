d993bd8fac18cc0b16ec73c95fd7ba1fb2a02b99
kaycodes13 <egad.coding@gmail.com>
2026-02-25 15:16:13 -0500

    Fixes #30: Force normal gravity for chapel maiden intro sequence

---
+++ Patches/IntroWeaknessPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Silksong.FsmUtil;
+using UnityEngine.SceneManagement;
+
+namespace Glissando.Patches;
+
+[HarmonyPatch(typeof(PlayMakerFSM), nameof(PlayMakerFSM.Start))]
+internal class IntroWeaknessPatch {
+	private static void Prefix(PlayMakerFSM __instance) {
+		if (
+			!__instance.gameObject.name.StartsWith("Weakness Scene")
+			|| SceneManager.GetActiveScene().name != "Bonetown"
+		) {
+			return;
+		}
+
+		__instance.GetState("Wait For Entry")?.InsertMethod(0, UnflipGravity);
+		__instance.GetState("Wait For Entry 2")?.InsertMethod(0, UnflipGravity);
+
+		static void UnflipGravity() {
+			if (GlissandoPlugin.GravityIsFlipped)
+				GlissandoPlugin.FlipGravity(HeroController.instance, force: true);
+		}
+	}
+}
